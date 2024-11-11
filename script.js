6fcf3afacecd4c24c0a64adafa2057e1c3c8dbab
RodrigoGonzalez10 <182010481+RodrigoGonzalez10@users.noreply.github.com>
2024-11-11 20:11:31 +0100

    Cambiar nombres

--- script.js
+++ script.js
@@ -86,4 +86,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+const menuIcon = document.querySelector('#menu-icon');
+const navbar= document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toogle('bx-x');
+    navbar.classList.toogle('active');
+}
+
+
 /**************************************************************/
